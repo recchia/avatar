31bbeab34cc85a9a3fbdfd3bde312346a70c85c7
Piero Recchia <piero.recchia@gmail.com>
2016-09-25 16:53:02 -0300

    Added AvatarControllerTest class for tests

--- tests/AppBundle/Controller/AvatarControllerTest.php
+++ tests/AppBundle/Controller/AvatarControllerTest.php
@@ -8,8 +8,28 @@
 
 namespace tests\AppBundle\Controller;
 
+use Faker\Factory;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class AvatarControllerTest
+class AvatarControllerTest extends WebTestCase
 {
+    public function testAvatarGet()
+    {
+        $faker = Factory::create();
+        $hash = md5($faker->email);
+        $url = '/avatars/' . $hash;
 
+        $client = static::createClient();
+        $client->request('GET', $url);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"'
+        );
+    }
 }
